efbb96853ca40d7c2f1325a53db715cf531ccd22
Octcarp <octcarp@hotmail.com>
2022-05-23 14:27:33 +0800

    countdown time cut

--- src/controller/Countdown.java
+++ src/controller/Countdown.java
@@ -7,8 +7,6 @@ import util.Step;
 import util.StepSaver;
 
 import javax.swing.*;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import static chessboard.ChessGameFrame.count;
 import static chessboard.ChessGameFrame.setResume;
@@ -16,55 +14,28 @@ import static chessboard.ChessGameFrame.setResume;
 public class Countdown extends Thread {
     JLabel j1 = count;
     Chessboard chessboard;
-    static long init = 30;
+    static long init = 15;
     public static long midTime = init;
     public static ChessColor color;
-    private ClickController controller;
-
-    /**
-     * 方式1
-     */
-    private static void time1() {
-        Timer timer = new Timer();
-        timer.schedule(new TimerTask() {
-            public void run() {
-                midTime--;
-                long ss = midTime % 60;
-                System.out.println("还剩" + ss + "秒");
-            }
-        }, 0, 1000);
-    }
 
     private final Object lock = new Object();
 
-    //标志线程阻塞情况
     private boolean pause = false;
 
-    /**
-     * 设置线程是否阻塞
-     */
     public void pauseThread() {
         this.pause = true;
     }
 
-    /**
-     * 调用该方法实现恢复线程的运行
-     */
     public void resumeThread() {
         this.pause = false;
         synchronized (lock) {
-            //唤醒线程
             lock.notify();
         }
     }
 
-    /**
-     * 这个方法只能在run 方法中实现，不然会阻塞主线程，导致页面无响应
-     */
     void onPause() {
         synchronized (lock) {
             try {
-                //线程 等待/阻塞
                 lock.wait();
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -75,8 +46,6 @@ public class Countdown extends Thread {
     @Override
     public void run() {
         super.run();
-        //标志线程开启
-        //一直循环
         while (true) {
             if (pause) {
                 onPause();
@@ -102,7 +71,7 @@ public class Countdown extends Thread {
     }
 
     public void changePause() {
-        if (pause == true) {
+        if (pause) {
             resumeThread();
             ChessGameFrame.setPause();
         } else {
@@ -115,10 +84,6 @@ public class Countdown extends Thread {
         midTime = init;
     }
 
-    /**
-     * 方式2
-     */
-
     public Countdown() {
 
     }
@@ -127,7 +92,7 @@ public class Countdown extends Thread {
         this.chessboard = chessboard;
     }
 
-
+    //本类有参考网络
 }
 
 
